643f2ec50a2dbd2d5dea54c0c23b673243e9f51b
Tom Ratcliff <ltratcliff@tombook-pro.local>
2023-10-19 10:49:54 -0400

    create html report in /tmp (vs relative)

--- scap_cmd.sh
+++ scap_cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Documentation here: https://redhatgov.io/workshops/rhel_8/exercise1.7/
-oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_stig --results-arf /tmp/arf.xml --report report.html /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml
+oscap xccdf eval --fetch-remote-resources --profile xccdf_org.ssgproject.content_profile_stig --results-arf /tmp/arf.xml --report /tmp/report.html /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml
